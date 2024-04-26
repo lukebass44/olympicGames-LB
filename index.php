0f71e977d5b529e9034db2d49a5e729d5cacabfc
lukebass44 <lbass@lclark.edu>
2024-04-26 16:26:35 +0000

    first phase: added and populated select menus

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
+<?php
+    include("src/functions.php");
+    $db = dbConnect();
+    $years = getYear($db);
+    $sports = getSports($db);
+    $cities = getCityNames($db);
+    $regions = getRegions($db);
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Fake Airbnb</title>
+    <title>Olympic Games</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
- 
     <link href="css/style.css" rel="stylesheet">
-    <link rel="icon" href="images/house-heart-fill.svg">
-    <link rel="mask-icon" href="images/house-heart-fill.svg" color="#000000">
-
   </head>
   <body>
 
@@ -20,16 +26,15 @@
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
                 <h4 class="text-white">About</h4>
-                <p class="text-muted">Fake Airbnb. Data c/o http://insideairbnb.com/get-the-data/</p>
+                <p class="text-muted">This is an Olymic games search engine. Use the select menus to find information about specific events or winners.</p>
                 </div>
             </div>
             </div>
         </div>
         <div class="navbar navbar-dark bg-dark shadow-sm">
             <div class="container">
-            <a href="index.php" class="navbar-brand d-flex align-items-center">
-                <i class="bi bi-house-heart-fill my-2"></i>    
-                <strong> Fake Airbnb</strong>
+            <a href="index.php" class="navbar-brand d-flex align-items-center">    
+                <strong> Olympic Games </strong>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -42,21 +47,101 @@
 
     <div class="album py-5 bg-light">
         <div class="container">
-        <h1>a title:</h1>
+            <h1>Search the Olympic Games database:</h1>
 
-            <div class="row g-3 align-items-center">
-                <div class="col-auto">
-                    <label for="neighborhood" class="col-form-label">sample form element label</label>
-                </div>
+            <!-- creates form that sends user to results webpage -->
+            <form action="results.php" method="GET">
 
-                <div class="col-auto">
-                        form element here
-                </div>
+                <div class="row g-3 align-items-center">
+                    <div class="col-auto">
+                        <label for="year" class="col-form-label">Year:</label>
+                    </div>
 
-            </div><!-- row -->
+                    <div class="col-auto">
+                        <select class="form-select" id="year" name="year">
+                            <option value='any'>Any</option>"
+                            <?php 
+                                // populates the year select menu
+                                foreach ($years as $year) {
+                                    $id = $year['id'];
+                                    $name = $year['games_name'];
+                                    echo "<option value='$id'>$name</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
 
+                </div><!-- row -->
+
+                <div class="row g-3 align-items-center">
+                    <div class="col-auto">
+                        <label for="sport" class="col-form-label">Sport:</label>
+                    </div>
+
+                    <div class="col-auto">
+                        <select class="form-select" id="sport" name="sport">
+                            <option value='any'>Any</option>"
+                            <?php 
+                                // populates the sports select menu
+                               foreach ($sports as $sport) {
+                                    $id = $sport['id'];
+                                    $name = $sport['sport_name'];
+                                    echo "<option value='$id'>$name</option>";
+                                } 
+                            ?>
+                        </select>
+                    </div>
+
+                </div><!-- row -->
+
+                <div class="row g-3 align-items-center">
+                    <div class="col-auto">
+                        <label for="city" class="col-form-label">City:</label>
+                    </div>
+
+                    <div class="col-auto">
+                        <select class="form-select" id="city" name="city">
+                            <option value='any'>Any</option>"
+                            <?php 
+                                // populates the city select menu
+                                foreach ($cities as $city) {
+                                    $id = $city['id'];
+                                    $name = $city['city_name'];
+                                    echo "<option value='$id'>$name</option>";
+                                }  
+                            ?>
+                        </select>
+                    </div>
+
+                </div><!-- row -->
+
+                <div class="row g-3 align-items-center">
+                    <div class="col-auto">
+                        <label for="region" class="col-form-label">Region:</label>
+                    </div>
+
+                    <div class="col-auto">
+                        <select class="form-select" id="region" name="region">
+                            <option value='any'>Any</option>"
+                            <?php 
+                                // populates the region select menu
+                                foreach ($regions as $region) {
+                                    $id = $region['id'];
+                                    $name = $region['region_name'];
+                                    echo "<option value='$id'>$name</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
+
+                </div><!-- row -->
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+            </form>
 
         </div><!-- .container-->
+
     </div><!-- album-->
 
     </main>
--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,9 @@
 
 /* Add your functions here */
 
+// have to include config.php for login credentials
+include ('/home/lbass/public_html/olympicGames-LB/config/config.php');
+
 
 function dbConnect(){
     /* defined in config/config.php */
@@ -23,5 +26,67 @@ function dbConnect(){
 }
 
 
+// fetch all of the games (includes year and season) on database
+function getYear($db)  {
+
+    try {
+        $stmt = $db->prepare("SELECT games_name FROM games ORDER BY games_name DESC");   
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    } 
+    catch (Exception $e) {
+        echo $e;
+    }
+
+}
+
+
+// gets all sports on database
+function getSports($db) {
+
+    try {
+        $stmt = $db->prepare("SELECT sport_name FROM sport ORDER BY sport_name");   
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    } 
+    catch (Exception $e) {
+        echo $e;
+    }    
+
+}
+
+
+// gets all the cities on db and alphabetizes them
+function getCityNames($db){
+
+    try {
+        $stmt = $db->prepare("SELECT city_name FROM city ORDER BY city_name");   
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    } 
+    catch (Exception $e) {
+        echo $e;
+    }    
+
+}
+
+
+// gets all countries/regions alphabetically to populate select menu
+function getRegions($db) {
+
+    try {
+        $stmt = $db->prepare("SELECT region_name FROM noc_region ORDER BY region_name");   
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    } 
+    catch (Exception $e) {
+        echo $e;
+    }
+
+}
 
 ?>
